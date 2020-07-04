d9a52a47d9cf587123ae676d98a708188a3ba3e1
elosiel guedes <elosiel@gmail.com>
2020-07-04 14:44:35 -0500

    array

--- index.js
+++ index.js
@@ -27,10 +27,15 @@ var originalFlavors = ["Banana Nut Fudge",
     "Raspberry Sherbet",
     "Rocky Road",
     "Strawberry",
-    "Vanilla",
+    "Vanilla",                                                                                                                                                                                                                                                                                                                                                           
     "Vanilla Burnt Almond"]
+     
+
+
+    
 
 /* Task 1: Confirm that the array is exactly 31 flavors. Your function should accept:
+
 
 (1) an array 
 
@@ -40,11 +45,19 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
+console.log(originalFlavors.length)
 
-    /*code here*/
+function is31Flavors(array){
+
+  if(array.length === 31) {
+    return true;
+  } else {
+      return false
+  }
 
 }
+
+console.log(is31Flavors(originalFlavors));
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,11 +70,15 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(array, flavor){
 
-    /*code here*/
+    flavor.unshift(array);
+
+    return originalFlavors;
 
 }
+
+console.log (addFlavor("Rainbow Sherbert", originalFlavors));
 
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
@@ -74,11 +91,14 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
+function removeLastFlavor(array){
 
-    /*code here*/
+    array.pop ();
+    return originalFlavors;
 
 }
+
+console.log(removeLastFlavor(originalFlavors));
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -89,12 +109,13 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(array, index){
 
-    /*code here*/
+    return array[index];
 
 }
 
+console.log(getFlavorByIndex(originalFlavors, 2));
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
 Your function should accept: 
