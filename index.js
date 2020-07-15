d4e723663a1b5dc75bbfcacfbc6531b7cfae1d8e
elosiel guedes <elosiel@gmail.com>
2020-07-14 20:54:26 -0500

    arrays

--- index.js
+++ index.js
@@ -109,13 +109,16 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(array, index){
+function getFlavorByIndex(arr, index){
 
-    return array[index];
+    return arr[index];
 
 }
 
 console.log(getFlavorByIndex(originalFlavors, 2));
+
+
+
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
 
 Your function should accept: 
@@ -129,11 +132,18 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
+function removeFlavorByname(array, flavour){
 
-    /*code here*/
+    for(let i = 0; i < array.length; i++){
+    if (arr[i] === flavour){
+         arr.splice(i, 1);
+         }
+    }
+    return originalFlavors;
 
 }
+
+console.log(removeFlavorByname(originalFlavors, "vanilla"));
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -144,11 +154,16 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+let originalFlavorsCopy = [];
 
-    /*code here*/
+function copy(originArr, newArr){
+
+    newArr = [...originArr];
+    return newArr;
 
 }
+
+console.log(copy(originalFlavors, originalFlavorsCopy));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
